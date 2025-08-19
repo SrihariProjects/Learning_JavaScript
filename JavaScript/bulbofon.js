68d5e8bc4387be8c11ff9fdef4069730c45a7179
Bestha Srihari <besthasrihari121@gmail.com>
2025-08-19 12:21:36 +0530

    My Learnings of first bulb code

---
+++ JavaScript/bulbofon.js
@@ -0,0 +1,10 @@
+// Get references to elements
+const imgref = document.getElementById("myImage");
+
+function turnOn() {
+  imgref.src = "../Images/pic_bulbon.gif";
+}
+
+function turnOff() {
+  imgref.src = "../Images/pic_bulboff.gif";
+}
---
+++ JavaScript/firstCode.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>what is the project name</h1>
+<p id="test">You can find by clicking below</p>
+<button type="button" onclick='document.getElementById("test").innerHTML="My first JS code!"'>
+Press Me!</button>
+</body>
+</html>
